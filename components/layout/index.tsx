f4748236353d6bad74bf41387b091b919d50b214
DragonScreech <104476247+DragonScreech@users.noreply.github.com>
2024-01-15 11:58:51 -0600

    Changed Title

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
     <Head>
-      <title>Frisco ISD HAC API</title>
+      <title>CFISD HAC API</title>
       <meta name="description" content="REST API to access student data from Home Access Center (Frisco ISD) "></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>

cd78862e02ccc4c76e402ed2e120f7a6f1732460
DragonScreech <104476247+DragonScreech@users.noreply.github.com>
2024-01-15 11:59:08 -0600

    Update index.tsx

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<Props> = ({ children }) => {
     <>
     <Head>
       <title>CFISD HAC API</title>
-      <meta name="description" content="REST API to access student data from Home Access Center (Frisco ISD) "></meta>
+      <meta name="description" content="REST API to access student data from Home Access Center"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
       <div className="bg-background flex flex-col h-full text-paragraph">
